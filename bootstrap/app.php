d0236f50efb3b49ab7795901789e7308edcc125d
ashianle <ashianle@users.noreply.github.com>
2026-09-14 11:09:40 +0700

    Connect frontend API access with CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->api(append: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
